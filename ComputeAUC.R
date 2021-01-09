a0efdc70b31e66590d73a17c849477b453e45cba
NxNiki <michaelniki1988@gmail.com>
2021-01-08 19:58:08 -0500

    Fixed dependent code

--- ComputeAUC.R
+++ ComputeAUC.R
@@ -1,4 +1,4 @@
-computeAUC = function(dt.g.all.sub.graph, covars.fmri, thresholds, metric, subjectColumn="Study.ID", small.dt, graph.dt){
+computeAUC = function(dt.g.all.sub.graph, covars.fmri, thresholds, metric, subjectColumn="Study.ID"){
 
     subjectStatus = covars.fmri[,.(SUBJID, status)]
     result = data.table(SUBJID=character(), metric = numeric())
@@ -12,9 +12,6 @@ computeAUC = function(dt.g.all.sub.graph, covars.fmri, thresholds, metric, subje
         for(i in 1:numThres){
             checkThreshold = thresholds[i]
             subjectResult[i] = dt.g.all.sub.graph[get(subjectColumn)==subjectID & threshold == checkThreshold][[metric]]
-            #if(small.dt[threshold==checkThreshold&SUBJID==subjectID]$sigma < 1 || 
-             #  graph.dt[threshold==checkThreshold&SUBJID==subjectID]$max.comp <= 235)
-             #   badThresholds = append(badThresholds, i);
         }
         if(length(badThresholds) > 0){
             auc = trapz(thresholds[-badThresholds], subjectResult[-badThresholds])
